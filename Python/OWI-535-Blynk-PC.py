6927e60ca85c82cd6a28b4a5a0f88112d59d7091
jctp <jamesctplant@gmail.com>
2020-04-13 16:32:51 +0100

    Merged multithreaded and master

--- Python/OWI-535-Blynk-PC.py
+++ Python/OWI-535-Blynk-PC.py
@@ -180,9 +180,17 @@ def serialComms():
     while True:
        blynk.run() 
 
+def defineTelemetry():
+    @blynk.VIRTUAL_READ(20)
+    def baseTelemetryHandler():
+        blynk.virtual_write(20, int(baseRot + baseRotOffset))
+
+
+
 serialLoop = threading.Thread(target = serialComms, args = ())
 serialLoop.start()
 
+defineTelemetry()
 defineButtonControls()
 
 while True:
