b8ea301aa1217931fb4e684b7e2fc2b36d341704
jctp <jamesctplant@gmail.com>
2020-04-13 13:17:52 +0100

    Added threading but it's broken

--- Python/OWI-535-Blynk-PC.py
+++ Python/OWI-535-Blynk-PC.py
@@ -1,36 +1,16 @@
 import serial
 import time
 import os
+import threading
+
 from roboarm import Arm
+arm = Arm()
 
 import BlynkLib
 BLYNK_AUTH = 'fe21DHbGkGWUTaTwSrwwViUyIFGWXSEc'
 blynk = BlynkLib.Blynk(BLYNK_AUTH)
 
-arm = Arm()
-
-ArduinoSerial = serial.Serial('/dev/ttyUSB0',9600)
-time.sleep(2)
-
-serialBuffer = ""
-
-baseRot = 0
-shoulderRot = 0
-elbowRot = 0
-
-baseRotOffset = 0
-shoulderRotOffset = 0
-elbowRotOffset = 0
-
-outputString = ""
-oldOutputString = ""
-cycles = 0
-
 def defineButtonControls():
-    @blynk.VIRTUAL_WRITE(0)
-    def emergencyStopHandler(value):
-        if int(value[0]) == 1:
-            arm.base.stop()
 
     @blynk.VIRTUAL_WRITE(1)
     def baseRotationHandlerCounter(value):
@@ -109,76 +89,101 @@ def defineButtonControls():
         if int(value[0]) == 0:
             arm.led.off()
 
-defineButtonControls()
+def serialComms():
 
-# Get input from Arduino serial and save it as current-session offset data
-while True:
+    ArduinoSerial = serial.Serial('/dev/ttyUSB0',9600)
+    time.sleep(2)
 
-    blynk.run()
-
-    serialBuffer = str(ArduinoSerial.readline())[2:-5]
-    if serialBuffer == "BASE":
-        serialBuffer = str(ArduinoSerial.readline())[2:-5]
-        baseRotOffset = int(serialBuffer)
-    if serialBuffer == "SHOULDER":
-        serialBuffer = str(ArduinoSerial.readline())[2:-5]
-        shoulderRotOffset = int(serialBuffer)
-    if serialBuffer == "ELBOW":
-        serialBuffer = str(ArduinoSerial.readline())[2:-5]
-        elbowRotOffset = int(serialBuffer)
     serialBuffer = ""
 
-    # Set up matrix for holding position data loaded from file
-    positionMemoryMatrix = [baseRot, shoulderRot, elbowRot]
+    baseRot = 0
+    shoulderRot = 0
+    elbowRot = 0
 
-    # On first run, load saved position
-    if cycles == 0:
+    baseRotOffset = 0
+    shoulderRotOffset = 0
+    elbowRotOffset = 0
 
-        print("CONTENTS OF MEMORY FILE:")
-        positionMemory = open('positionMemory.csv', 'r')
-        positionMemoryMatrix = positionMemory.read()
+    outputString = ""
+    oldOutputString = ""
+    cycles = 0
+
+    while True:
+        serialBuffer = str(ArduinoSerial.readline())[2:-5]
+        if serialBuffer == "BASE":
+            serialBuffer = str(ArduinoSerial.readline())[2:-5]
+            baseRotOffset = int(serialBuffer)
+        if serialBuffer == "SHOULDER":
+            serialBuffer = str(ArduinoSerial.readline())[2:-5]
+            shoulderRotOffset = int(serialBuffer)
+        if serialBuffer == "ELBOW":
+            serialBuffer = str(ArduinoSerial.readline())[2:-5]
+            elbowRotOffset = int(serialBuffer)
+        serialBuffer = ""
+
+        # Set up matrix for holding position data loaded from file
+        positionMemoryMatrix = [baseRot, shoulderRot, elbowRot]
+
+        # On first run, load saved position
+        if cycles == 0:
+
+            print("CONTENTS OF MEMORY FILE:")
+            positionMemory = open('positionMemory.csv', 'r')
+            positionMemoryMatrix = positionMemory.read()
+            positionMemory.close()
+
+            # Split CSV
+            positionMemoryMatrix = positionMemoryMatrix.split(",")
+            print(positionMemoryMatrix)
+
+            baseRot = int(positionMemoryMatrix[0])
+            shoulderRot = int(positionMemoryMatrix[1])
+            elbowRot = int(positionMemoryMatrix[2])
+            print("LOADED: B:" + str(baseRot) + " S:" + str(shoulderRot) + " E:" + str(elbowRot))
+
+        # Save ongoing position data
+        positionMemory = open('positionMemory.csv', 'w')
+        positionMemoryMatrixFormatted = str(baseRot + baseRotOffset) + "," + str(shoulderRot + shoulderRotOffset) + "," + str(elbowRot + elbowRotOffset)
+        positionMemory.write(positionMemoryMatrixFormatted)
         positionMemory.close()
 
-        # Split CSV
-        positionMemoryMatrix = positionMemoryMatrix.split(",")
-        print(positionMemoryMatrix)
+        # Make copy of output string for comparing for differences and deciding if to print
+        oldOutputString = outputString
 
-        baseRot = int(positionMemoryMatrix[0])
-        shoulderRot = int(positionMemoryMatrix[1])
-        elbowRot = int(positionMemoryMatrix[2])
-        print("LOADED: B:" + str(baseRot) + " S:" + str(shoulderRot) + " E:" + str(elbowRot))
+        #Loaded from file
+        outputStringBase = "B:" + str(baseRot) + " S:" + str(shoulderRot) + " E:" + str(elbowRot)
 
-    # Save ongoing position data
-    positionMemory = open('positionMemory.csv', 'w')
-    positionMemoryMatrixFormatted = str(baseRot + baseRotOffset) + "," + str(shoulderRot + shoulderRotOffset) + "," + str(elbowRot + elbowRotOffset)
-    positionMemory.write(positionMemoryMatrixFormatted)
-    positionMemory.close()
+        # Movement for this session
+        outputStringOffsets = "B:" + str(baseRotOffset) + " S:" + str(shoulderRotOffset) + " E:" + str(elbowRotOffset)
 
-    # Make copy of output string for comparing for differences and deciding if to print
-    oldOutputString = outputString
+        # Combine offsets and base
+        outputString = "B:" + str(baseRot + baseRotOffset) + " S:" + str(shoulderRot + shoulderRotOffset) + " E:" + str(elbowRot + elbowRotOffset)
 
-    #Loaded from file
-    outputStringBase = "B:" + str(baseRot) + " S:" + str(shoulderRot) + " E:" + str(elbowRot)
+        # Print UI
+        if outputString != oldOutputString:
+            os.system('clear')
 
-    # Movement for this session
-    outputStringOffsets = "B:" + str(baseRotOffset) + " S:" + str(shoulderRotOffset) + " E:" + str(elbowRotOffset)
+            print("BASE VALUES:")
+            print(outputStringBase)
+            print()
+            print("OFFSETS:")
+            print(outputStringOffsets)
+            print()
+            print("COMBINED:")
+            print(outputString)
+            print()
+            print("Cycle count: " + str(cycles))
 
-    # Combine offsets and base
-    outputString = "B:" + str(baseRot + baseRotOffset) + " S:" + str(shoulderRot + shoulderRotOffset) + " E:" + str(elbowRot + elbowRotOffset)
+        cycles = cycles + 1
 
-    # Print UI
-    if outputString != oldOutputString:
-        os.system('clear')
+    defineButtonControls()
+    while True:
+       blynk.run() 
 
-        print("BASE VALUES:")
-        print(outputStringBase)
-        print()
-        print("OFFSETS:")
-        print(outputStringOffsets)
-        print()
-        print("COMBINED:")
-        print(outputString)
-        print()
-        print("Cycle count: " + str(cycles))
+#serialLoop = threading.Thread(target = serialComms, args = ())
+#serialLoop.start()
 
-    cycles = cycles + 1
+defineButtonControls()
+
+while True:
+    blynk.run()
